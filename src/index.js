a923b0bb5053febc0eb01b65d187b2ba62fd54f6
Div685 <divyeshpatel685@gmail.com>
2021-03-03 22:57:06 +0300

    Add style to page
    - Added font-awesome icons
    - Added babel-loader, file-loader dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/style.css';
 
 console.log('Hello Todos');
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,26 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './dist/index.html',
-    }),
-  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.(css|sass|scss)$/,
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: ['file-loader']
+      }
     ],
   },
 }
